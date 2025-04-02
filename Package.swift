c405faed076d4e685b53e3e5a7b7f3194d0f7f58
Edwin Pena <epena@fintonic.com>
2025-04-02 09:26:39 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/edwinps/pokemonApp/releases/assets/242560956",
+            url: "https://github.com/edwinps/pokemonApp/releases/assets/242560956.zip",
             checksum: "3ed98867c128e28f63d1dba60efcabafdaafa398ee6126affcd07653d30c32c6"
         )
     ]
